266efc0f97aedce26c0b30fa59c0a882b2ac57b8
qc <cqbus405@gmail.com>
2017-01-10 23:41:31 +0800

    redis

--- src/config/settings.js
+++ src/config/settings.js
@@ -2,12 +2,13 @@ export default {
   development: {
     ip: '66.1.33.112',
     port: 3001,
-    mysql: 'mysql://root:gogogo@localhost/test',
+    mysql: 'mysql://root:cqgogogo@localhost/test',
     salt_round: 10,
     token_secret: '%lindsay27!',
     redis: {
-      host: '66.1.33.112',
-      port: 6380
+      // host: '66.1.33.112',
+      host: '127.0.0.1',
+      port: 6379
     }
   },
   prod: {
