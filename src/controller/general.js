2ec75d8eacdbe588b08a5c40c278e50eca351626
qc <cqbus405@gmail.com>
2017-01-22 17:31:11 +0800

    captcha size

--- src/controller/general.js
+++ src/controller/general.js
@@ -8,13 +8,13 @@ import {
 export function generateCaptcha(req, res) {
   const captcha = create({
     width: 80,
-    height: 31,
+    height: 39,
     size: 4, // size of random string
     ignoreChars: '0o1i', // filter out some characters like 0o1i
     noise: 2, // number of noise lines
     color: false, // characters will have distinct colors instead of grey, true if background option is set
-  // background: 'rgba(255, 255, 255, 0.87)' // background color of the svg image
-})
+    // background: 'rgba(255, 255, 255, 0.87)' // background color of the svg image
+  })
 
   const newCaptcha = captcha.data.replace(/ <path/g, '<path')
   const text = captcha.text
