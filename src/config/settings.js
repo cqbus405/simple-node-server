b348df9205f39621e4683b71bd55c36006b08e1f
qc <cqbus405@gmail.com>
2017-03-26 14:59:01 +0800

    saf

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,17 +1,18 @@
 export default {
   development: {
-    ip: '66.1.33.112',
+    // ip: '66.1.33.112',
+    ip: '127.0.0.1',
     port: 3001,
-    mysql: 'mysql://root:gogogo@localhost/test',
+    mysql: 'mysql://root:cqgogogo@localhost/test',
     salt_round: 10,
     token_secret: '%lindsay27!',
     redis: {
-      host: '66.1.33.112',
-      // host: '127.0.0.1',
+      // host: '66.1.33.112',
+      host: '127.0.0.1',
       port: 6379
     },
     formidable: {
-      uploadDir: 'D:/upload',
+      uploadDir: '../../upload',
       keepExtensions: true,
       maxFieldsSize: 50 * 1024 * 1024,
       maxFields: 9
